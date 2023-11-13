00b3b8e2bcd53ee2b30c8d7f49d4c97c0d573028
smolkinakate17 <smolkinakate17@gmail.com>
2023-11-13 22:33:26 +0400

    исправила ошибки в задачах 2.1 и 2.2

--- src/TwoDotOne.java
+++ src/TwoDotOne.java
@@ -7,8 +7,8 @@ public class TwoDotOne {
         in.useLocale(Locale.US);
         double price = in.nextDouble();
         int kopeckPrice = (int) (price * 100);
-        int kopeckWithoutNds = (int) Math.round((double) (kopeckPrice * 100) / 120);
-        int kopeckNds = (int) Math.round((double) (kopeckPrice * 20) / 120);
+        int kopeckWithoutNds = (kopeckPrice * 100) / 120;
+        int kopeckNds = (int) Math.round((double)(kopeckPrice * 20) / 120);
         System.out.println("Стоимость товара без НДС " + (double) kopeckWithoutNds / 100);
         System.out.println("Сумма НДС в чеке " + (double) kopeckNds / 100);
         System.out.println("Сумма НДС для налоговой декларации " + Math.round((double) kopeckNds / 100));
--- src/TwoDotTwo.java
+++ src/TwoDotTwo.java
@@ -13,58 +13,56 @@ public class TwoDotTwo {
         int hours = n / 60;
         n %= 60;
         int minutes = n;
-        String strDays = getDaysStr(days);
-        String strHours = getHoursStr(hours);
-        String strMinutes = getMinutesStr(minutes);
 
-        System.out.println(nCopy + " - это " + strDays + ", " + strHours + ", " + strMinutes + ".");
+        System.out.println(nCopy + getTimeStr(days, hours, minutes));
 
         return 0;
     }
 
-    private static String getDaysStr(int days) {
-        String d = String.valueOf(days);
+    private static String getTimeStr(int days, int hours, int minutes) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(" - это ");
 
-        if (d.charAt(0) == '1' && d.length() == 2) {
-            return days + " дней";
-        }
-        if (days % 10 == 1) {
-            return days + " день";
-        }
-        if (days % 10 == 2 || days % 10 == 3 || days % 10 == 4) {
-            return days + " дня";
+        int temp = days / 10;
+        if (temp > 9) {
+            stringBuilder.append(days).append(" дней");
+        } else if (temp == 1) {
+            stringBuilder.append(days).append(" дней");
+        } else if (days % 10 == 1) {
+            stringBuilder.append(days).append(" день");
+        } else if (days % 10 == 2 || days % 10 == 3 || days % 10 == 4) {
+            stringBuilder.append(days).append(" дня");
         } else {
-            return days + " дней";
+            stringBuilder.append(days).append(" дней");
         }
+        stringBuilder.append(", ");
+
+        temp = hours / 10;
+        if (temp == 1) {
+            stringBuilder.append(hours).append(" часов");
+
+        } else if (hours % 10 == 1) {
+            stringBuilder.append(hours).append(" час");
+        } else if (hours % 10 == 2 || hours % 10 == 3 || hours % 10 == 4) {
+            stringBuilder.append(hours).append(" часа");
+        } else {
+            stringBuilder.append(hours).append(" часов");
+        }
+        stringBuilder.append(", ");
+
+        temp = minutes / 10;
+        if (temp == 1) {
+            stringBuilder.append(minutes).append(" минут");
+        } else if (minutes % 10 == 1) {
+            stringBuilder.append(minutes).append(" минута");
+        } else if (minutes % 10 == 2 || minutes % 10 == 3 || minutes % 10 == 4) {
+            stringBuilder.append(minutes).append(" минуты");
+        } else {
+            stringBuilder.append(minutes).append(" минут");
+        }
+        stringBuilder.append(".");
+        return stringBuilder.toString();
     }
 
-    private static String getHoursStr(int hours) {
-        String h = String.valueOf(hours);
-        if (h.charAt(0) == 1 && h.length() == 2) {
-            return hours + " часов";
-        }
-        if (hours % 10 == 1) {
-            return hours + " час";
-        }
-        if (hours % 10 == 2 || hours % 10 == 3 || hours % 10 == 4) {
-            return hours + " часа";
-        } else {
-            return hours + " часов";
-        }
-    }
 
-    private static String getMinutesStr(int minutes) {
-        String m = String.valueOf(minutes);
-        if (m.charAt(0) == 1 && m.length() == 2) {
-            return minutes + " минут";
-        }
-        if (minutes % 10 == 1) {
-            return minutes + " минута";
-        }
-        if (minutes % 10 == 2 || minutes % 10 == 3 || minutes % 10 == 4) {
-            return minutes + " минуты";
-        } else {
-            return minutes + " минут";
-        }
-    }
 }
